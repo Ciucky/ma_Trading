e5a67e7b4073e7c5913b3f7c88b2edf936a80747
Ciucky <34582885+Ciucky@users.noreply.github.com>
2022-04-01 01:32:40 +0100

    First commit

---
+++ ma_trading_signals/src/ma_trading_signals/Apple_signal.java
@@ -0,0 +1,10 @@
+package ma_trading_signals;
+
+public class Apple_signal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ma_trading_signals/src/module-info.java
@@ -0,0 +1,2 @@
+module ma_trading_signals {
+}
