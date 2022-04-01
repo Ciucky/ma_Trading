392af601bbd4700b36f64175e68bfcc1d218016a
Ciucky <34582885+Ciucky@users.noreply.github.com>
2022-04-01 02:43:41 +0100

    Saved the data in a hashmap. Key = date, Value = the rest

--- ma_trading_signals/src/ma_trading_signals/Apple_signal.java
+++ ma_trading_signals/src/ma_trading_signals/Apple_signal.java
@@ -7,16 +7,46 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List; 
+import java.util.HashMap;
 
 public class Apple_signal {
 
 	public static void main(String[] args) {
+		int c = 0;
 		// TODO Auto-generated method stub
-		List<List<String>> apple = new ArrayList<List<String>>();
+		HashMap<String, List<List<String>>> apple = new HashMap<String, List<List<String>>>();
 		try (CSVReader csvReader = new CSVReader(new FileReader("HistoricalQuotes.csv"));) {
 		    String[] values = null;
 		    while ((values = csvReader.readNext()) != null) {
-		        apple.add(Arrays.asList(values));
+				List<String> arr = new ArrayList<String>();
+		    	for(int i = 1; i <= 5; i++) {
+		        arr.add(values[i]);
+		    	}		
+		    	//System.out.println(arr);
+		        //array.add(Arrays.asList(arr));
+		        
+		        apple.put(values[0], Arrays.asList(arr));
+		        
+		        /*
+		        if(c < 3) {
+				//System.out.println(values[0]);
+		        System.out.println();
+			    System.out.println(c);
+			    System.out.println();
+			    System.out.print("values = ");
+			    for(int i = 0; i< values.length; i++) {
+			    System.out.print(values[i]);
+			    }
+			    System.out.println();
+		        System.out.println("arr= " + arr);
+		        System.out.println();
+		        System.out.println("apple =" + apple);
+		        }
+		        c = c+1;
+		    	//Arrays.fill( values, null );
+		    	values = new String[values.length];
+		    	*/
+
 		    }
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -25,7 +55,7 @@ public class Apple_signal {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(apple.get(1).get(1));
+		System.out.println(apple.get("02/28/2020"));
 	}
 
 }
